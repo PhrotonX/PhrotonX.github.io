9e5c0c9259b38e250006ce53b66ceba20204bf1d
Charles Martin <cpascual185@gmail.com>
2024-04-09 22:55:57 +0800

    24w14c: Replace Project content with JSON

--- js/script.js
+++ js/script.js
@@ -16,16 +16,48 @@ function navOnNewtab(url){
 }
 
 //https://gist.github.com/PhrotonX/98e9786c1f67de6de59bd4662f074e82#file-projects-json
+/**
+ * itemName must be singular.
+ * @param {string} itemName 
+ * @param {string} filename 
+ */
 function loadItems(itemName, filename){
     fetch(filename)
         .then((response) => response.json())
         .then((json) => {
-            // alert(json.item);
-            //console.log(json);
+            var itemGroup = document.getElementById(itemName + "s");
+            for(let i = 0; i < json.size; i++){
+                var itemElement = document.createElement("div");
+                itemElement.setAttribute("class", itemName + " item");
+                itemElement.setAttribute("id", itemName + "-" + i);
+                
+                if((json.items[i].image != "") || (json.items[i].image != null)){
+                    var itemImage = document.createElement("img");
+                    itemImage.setAttribute("class", "item-image");
+                    itemImage.setAttribute("id", "item-image-" + i);
+                    itemImage.setAttribute("src", json.items[i].image);
+                    itemElement.appendChild(itemImage);
+                    //document.getElementById("item-image-" + "1").setAttribute("src", json.items[i].image);
+                }
 
-            // const data = JSON.parse(json);
+                var itemContent = document.createElement("section");
+                itemContent.setAttribute("class", "item-content acrylic parent shadow");
+                itemElement.appendChild(itemContent);
 
-            // const id = json[0].title;
-            document.getElementById(itemName + "-1").innerHTML = "<p>"+ json.items[0].title +"</p>";
+                itemContent.innerHTML = "<h3 class='title'>"+ json.items[i].title +"</h3>";
+                if((json.items[i].date[0].progress != null) || (json.items[i].date[0].progress != "")){
+                    itemContent.innerHTML += "<p class='date'>"+ json.items[i].date[0].month + " " + json.items[i].date[0].year + "-" + json.items[i].date[0].progress + "</p>";
+                }else{
+                    itemContent.innerHTML += "<p class='date'>"+ json.items[i].date[0].month + " " + json.items[i].date[0].year + "</p>";
+                }
+
+                itemGroup.appendChild(itemElement);             
+
+                /*document.getElementById(itemName + "-" + "1").innerHTML = "<p>"+ json.items[i].title +"</p>";
+                */
+            }
+            
+
+            
         });
 }
