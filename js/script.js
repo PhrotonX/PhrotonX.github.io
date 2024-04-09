9855ed11cfacd7f436ec38f9e6296a3550856c5f
Charles Martin <cpascual185@gmail.com>
2024-04-09 10:17:45 +0800

    24w14b: Replace Project content with JSON

--- js/script.js
+++ js/script.js
@@ -14,3 +14,18 @@ function loadPagePart(filename, containerId){
 function navOnNewtab(url){
     window.open(url, '_blank');
 }
+
+//https://gist.github.com/PhrotonX/98e9786c1f67de6de59bd4662f074e82#file-projects-json
+function loadItems(itemName, filename){
+    fetch(filename)
+        .then((response) => response.json())
+        .then((json) => {
+            // alert(json.item);
+            //console.log(json);
+
+            // const data = JSON.parse(json);
+
+            // const id = json[0].title;
+            document.getElementById(itemName + "-1").innerHTML = "<p>"+ json.items[0].title +"</p>";
+        });
+}
