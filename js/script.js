0f72c086ce8d5b698d0561a2918904586f456aa3
Charles Martin <cpascual185@gmail.com>
2024-04-11 12:16:35 +0800

    24w14d: Replace More Project content with JSON

--- js/script.js
+++ js/script.js
@@ -11,6 +11,10 @@ function loadPagePart(filename, containerId){
         })
 }
 
+function navigate(url, target){
+    window.open(url, target);
+}
+
 function navOnNewtab(url){
     window.open(url, '_blank');
 }
@@ -25,36 +29,82 @@ function loadItems(itemName, filename){
     fetch(filename)
         .then((response) => response.json())
         .then((json) => {
+            const items = Object.keys(json.item).length;
+
             var itemGroup = document.getElementById(itemName + "s");
-            for(let i = 0; i < json.size; i++){
+
+            for(let i = 0; i < items; i++){
                 var itemElement = document.createElement("div");
                 itemElement.setAttribute("class", itemName + " item");
                 itemElement.setAttribute("id", itemName + "-" + i);
                 
-                if((json.items[i].image != "") || (json.items[i].image != null)){
+                if((json.item[i].image != "") || (json.item[i].image != null)){
                     var itemImage = document.createElement("img");
                     itemImage.setAttribute("class", "item-image");
                     itemImage.setAttribute("id", "item-image-" + i);
-                    itemImage.setAttribute("src", json.items[i].image);
+                    itemImage.setAttribute("src", json.item[i].image);
                     itemElement.appendChild(itemImage);
-                    //document.getElementById("item-image-" + "1").setAttribute("src", json.items[i].image);
+                    //document.getElementById("item-image-" + "1").setAttribute("src", json.item[i].image);
                 }
 
                 var itemContent = document.createElement("section");
                 itemContent.setAttribute("class", "item-content acrylic parent shadow");
-                itemElement.appendChild(itemContent);
+                
+                itemContent.innerHTML = "<h3 class='title'>"+ json.item[i].title +'</h3><p class="category">'+json.item[i].category+"</p>";
 
-                itemContent.innerHTML = "<h3 class='title'>"+ json.items[i].title +"</h3>";
-                if((json.items[i].date[0].progress != null) || (json.items[i].date[0].progress != "")){
-                    itemContent.innerHTML += "<p class='date'>"+ json.items[i].date[0].month + " " + json.items[i].date[0].year + "-" + json.items[i].date[0].progress + "</p>";
+                if((json.item[i].date[0].progress != null) || (json.item[i].date[0].progress != "")){
+                    itemContent.innerHTML += "<p class='date'>"+ json.item[i].date[0].month + " " + json.item[i].date[0].year + "-" + json.item[i].date[0].progress + "</p>";
                 }else{
-                    itemContent.innerHTML += "<p class='date'>"+ json.items[i].date[0].month + " " + json.items[i].date[0].year + "</p>";
+                    itemContent.innerHTML += "<p class='date'>"+ json.item[i].date[0].month + " " + json.item[i].date[0].year + "</p>";
+                }
+ 
+                var itemDescription = document.createElement("ul");
+                itemDescription.setAttribute("class", "item-description");
+                itemDescription.setAttribute("id", "item-description-" + i);
+                
+                const itemDescriptionCount = Object.keys(json.item[i].description).length;
+                for(let j = 0; j < itemDescriptionCount; j++){
+                    var itemDescriptionListItem = document.createElement("li");
+                    itemDescriptionListItem.innerHTML += "<p>"+ json.item[i].description[j] +"</p>";
+                    
+                    itemDescription.appendChild(itemDescriptionListItem);
                 }
 
-                itemGroup.appendChild(itemElement);             
+                itemContent.appendChild(itemDescription);
 
-                /*document.getElementById(itemName + "-" + "1").innerHTML = "<p>"+ json.items[i].title +"</p>";
+                if(json.item[i].note != null){
+                    const itemNotesCount = Object.keys(json.item[i].note).length;
+                    for(let j = 0; j < itemNotesCount; j++){
+                        var itemNote = document.createElement("p");
+                        itemNote.innerHTML += json.item[i].note[j];
+
+                        itemContent.appendChild(itemNote);
+                    }
+                }
+                
+                if(json.item[i].button != null){
+                    const itemButtonCount = Object.keys(json.item[i].button).length;
+                    for(let j = 0; j < itemButtonCount; j++){
+                        var itemButton = document.createElement("button");
+                        if((json.item[i].button[j].action != null) && (json.item[i].button[j].target != null)){
+                            var link = json.item[i].button[j].action;
+                            itemButton.setAttribute("onclick", "navigate('" +
+                                link.slice(5) + "', '" + json.item[i].button[j].target + "')");
+                        }
+                        itemButton.setAttribute("type", "button");
+                        if(json.item[i].button[j].content != null){
+                            itemButton.innerHTML += json.item[i].button[j].content;
+                        }
+
+                        itemContent.appendChild(itemButton);
+                    }
+                }
+
+                /*document.getElementById(itemName + "-" + "1").innerHTML = "<p>"+ json.item[i].title +"</p>";
                 */
+               
+                itemElement.appendChild(itemContent);
+                itemGroup.appendChild(itemElement);
             }
             
 
